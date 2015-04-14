d892e2a264e522707470e061d0fded6e1d0a0040
Ian Harrigan <ianharrigan@hotmail.com>
2015-04-14 13:58:14 +0200

    Initial commit

---
+++ src/haxeui/flashdevelop/preview/Main.hx
@@ -0,0 +1,31 @@
+package haxeui.flashdevelop.preview;
+
+import haxe.ui.toolkit.core.Root;
+import haxe.ui.toolkit.core.Toolkit;
+import haxe.ui.toolkit.themes.GradientTheme;
+import openfl.external.ExternalInterface;
+
+class Main {
+	private static var _main:MainController;
+	
+	public static function main() {
+		Toolkit.theme = new GradientTheme();
+		Toolkit.init();
+		Toolkit.addStyleSheet("css/main.css");
+		Toolkit.open(function(root:Root) {
+			_main = new MainController();
+			try {
+				ExternalInterface.addCallback("updateLayout", function(layoutString:String) {
+					_main.updateLayout(layoutString);
+				});
+				
+				ExternalInterface.call("callbacksReady");
+			} catch (e:Dynamic) {
+				trace("ERROR: " + e);
+			}
+
+			
+			root.addChild(_main.view);
+		});
+	}
+}
