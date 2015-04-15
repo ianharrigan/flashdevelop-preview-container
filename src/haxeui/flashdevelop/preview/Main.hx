14f2cc6b1dcfffe22a0dfa7d009c43c63400ac3c
Ian Harrigan <ianharrigan@hotmail.com>
2015-04-15 22:55:24 +0200

    Allow predifined theme changes

--- src/haxeui/flashdevelop/preview/Main.hx
+++ src/haxeui/flashdevelop/preview/Main.hx
@@ -1,13 +1,20 @@
 package haxeui.flashdevelop.preview;
 
+import format.svg.Grad;
+import haxe.ui.toolkit.core.interfaces.InvalidationFlag;
 import haxe.ui.toolkit.core.Root;
 import haxe.ui.toolkit.core.Toolkit;
 import haxe.ui.toolkit.resources.ResourceManager;
+import haxe.ui.toolkit.style.StyleManager;
+import haxe.ui.toolkit.themes.DefaultTheme;
+import haxe.ui.toolkit.themes.GradientMobileTheme;
 import haxe.ui.toolkit.themes.GradientTheme;
+import haxe.ui.toolkit.themes.Theme;
 import openfl.external.ExternalInterface;
 
 class Main {
 	private static var _main:MainController;
+	private static var _layoutString:String;
 	private static var _originalTrace = haxe.Log.trace;
 	
 	public static function main() {
@@ -20,7 +27,31 @@ class Main {
 			_main = new MainController();
 			try {
 				ExternalInterface.addCallback("updateLayout", function(layoutString:String) {
+					_layoutString = layoutString;
 					_main.updateLayout(layoutString);
+				});
+
+				ExternalInterface.addCallback("setTheme", function(theme:String) {
+					trace("Setting theme to: " + theme);
+					StyleManager.instance.clear();
+					var t:Theme = null;
+					
+					switch (theme) { // TODO: make this dynamic once theme system has been improved (Issue #121)
+						case "Default":
+							t = new DefaultTheme();
+						case "Gradient":
+							t = new GradientTheme();
+						case "Gradient Mobile":
+							t = new GradientMobileTheme();
+					}
+					
+					if (t != null) {
+						t.apply();
+						root.removeAllChildren();
+						_main = new MainController();
+						root.addChild(_main.view);
+						_main.updateLayout(_layoutString);
+					}
 				});
 				
 				ExternalInterface.addCallback("redirectTrace", function(redirect:String) {
